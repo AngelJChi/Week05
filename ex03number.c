9b22f37a7367d2acd8861d4999896dd0faadb73c
AngelJChi <2009039@upy.edu.mx>
2021-06-06 16:06:37 -0500

    ex03

---
+++ ex03number.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+int main(){
+	float n = 0;
+
+	printf("Calcular n");
+	scanf(%f,&n);
+
+	n = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + 4/13 - 4/15 + 4/17 - 4/19;
+
+	printf("El resulrado de n es %f, n");
+
+	return = 0;
+
+}
