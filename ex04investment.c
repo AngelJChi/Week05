5219a2b98b4705e942ef1505c496aa1ed39f84a6
AngelJChi <2009039@upy.edu.mx>
2021-06-07 14:13:21 -0500

    ex04

---
+++ ex04investment.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	int inv1,inv2,inv3,total = 0;
+
+	prinft("Escribe la inversion de la persona 1");
+	scanf("%i",&inv1);
+	prinft("Escribe la inversion de la persona 2");
+        scanf("%i",&inv2);
+	prinft("Escribe la inversion de la persona 3");
+        scanf("%i",&inv3);
+
+	total = (inv1 + inv2 + inv3)
+
+
+	prinft("El procentaje de la persona 1 es: (inv1/total)*100%", inv1);
+	prinft("El procentaje de la persona 2 es: (inv2/total)*100%", inv1);
+	prinft("El procentaje de la persona 3 es: (inv3/total)*100%", inv1);
+
+
+	return 0;
+}
