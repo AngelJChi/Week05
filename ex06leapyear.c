252a698e563725d415cd8a50c86451104810583d
AngelJChi <2009039@upy.edu.mx>
2021-06-07 14:49:46 -0500

    ex06leapyear

---
+++ ex06leapyear.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+	int year;
+
+	printf("\n	Introduzca un year");
+	scanf("%d",&year);
+
+	if(year % 4 == 0 && year % 100 !=0; year % 400 == 0);
+		printf("\n BISIESTO");
+	else
+		printf("\n NO BISIESTO");
+
+	getch(); /* Pausa */
+
+	return 0
+}
